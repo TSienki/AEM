3e7031ba567013711a6dcf208d723774bbfb5174
Sienki <zonced@gmail.com>
2019-03-10 14:21:52 +0100

    Code refactor, add time measurements

--- Lab1.py
+++ Lab1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import MST
 from prim import prim, prim_n_tree_generate
 import random
+import time
 
 
 def euclidean_distance(point_1, point_2):
@@ -27,23 +28,72 @@ def create_matrix(data, dist_fun):
     return matrix
 
 
-def create_one_tree_prim(data, dist_matrix):
-    starting_point = random.randint(0, len(data))
-    result = prim(dist_matrix, starting_point)
+def time_measure(func, args_for_func):
+    """
+    :param func:
+    :param args_for_func:
+    :return: time in seconds
+    """
+    start = time.time()
+    result = func(*args_for_func)
+    end = time.time()
+    return end - start, result
+
+
+def create_n_trees_kruskal(data, dist_matrix, step_to_time_measure=1):
+    g = MST.Graph(np.size(dist_matrix))
+    for i in range(dist_matrix.shape[0]):
+        for j in range(dist_matrix.shape[1]):
+            g.add_edge(i, j, dist_matrix[i][j])
+
+    result = g.kruskal_mst()
+    cost = 0
+    for u, v, weight in result[:len(result)-9]:
+         plt.plot([data[u].T[0], data[v].T[0]], [data[u].T[1], data[v].T[1]], 'k-')
+         cost += weight
+
+    print("Minimalne drzewo rozpinajace: " + str(cost))
+
+    plt.scatter(data.T[0], data.T[1])
+    plt.xlim(-10, 260)
+    plt.ylim(-10, 260)
+    plt.show()
+
+
+def create_one_tree_prim(data, dist_matrix, steps_for_time_measurements=1):
+    times_measurements = []
+    for i in range(steps_for_time_measurements):
+        starting_point = random.randint(0, len(data) - 1)
+        measurement, result = time_measure(prim, (dist_matrix, starting_point))
+        times_measurements.append(measurement)
 
     visualise_tree(data, result, starting_point)
+    cost = count_cost(result, dist_matrix)
+    print(f"Minimalne drzewo rozpinajace: {cost}")
+    print(f"Pomiary czasu dla {steps_for_time_measurements} kroków to "
+          f"min: {min(times_measurements)} sekund, max: {max(times_measurements)} sekund i "
+          f"avg: {sum(times_measurements)/len(times_measurements)} sekund")
 
 
-def create_n_trees_prim(data, dist_matrix):
-    starting_points_indices = np.random.choice(data.shape[0],
-                                               10,
-                                               replace=False)
-    starting_points_indices = np.sort(starting_points_indices)
-    mask = np.ones(data.shape[0], dtype=bool)
-    mask[starting_points_indices] = False
+def create_n_trees_prim(data, dist_matrix, steps_for_time_measurements=1):
+    times_measurements = []
+    for i in range(steps_for_time_measurements):
+        starting_points_indices = np.random.choice(data.shape[0],
+                                                   10,
+                                                   replace=False)
+        starting_points_indices = np.sort(starting_points_indices)
+        mask = np.ones(data.shape[0], dtype=bool)
+        mask[starting_points_indices] = False
+        measurement, result = time_measure(prim_n_tree_generate, (dist_matrix, starting_points_indices))
+        times_measurements.append(measurement)
 
-    result = prim_n_tree_generate(dist_matrix, starting_points_indices)
     visualise_n_trees(data, result, mask)
+
+    cost = count_cost(result, dist_matrix)
+    print(f"Minimalne drzewo rozpinajace: {cost}")
+    print(f"Pomiary czasu dla {steps_for_time_measurements} kroków to "
+          f"min: {min(times_measurements)} sekund, max: {max(times_measurements)} sekund i "
+          f"avg: {sum(times_measurements)/len(times_measurements)} sekund")
 
 
 def visualise_tree(points, nodes, starting_point):
@@ -68,6 +118,13 @@ def visualise_n_trees(points, nodes, mask):
     plt.show()
 
 
+def count_cost(nodes, cost_matrix):
+    cost = 0
+    for node in nodes:
+        cost += cost_matrix[node[0], node[1]]
+    return cost
+
+
 def read_data(f_name):
     data = []
     with open(f_name) as f:
@@ -84,8 +141,9 @@ def read_data(f_name):
 def main_function():
     data = read_data("objects.data")
     dist_matrix = create_matrix(data, euclidean_distance)
-    create_one_tree(data, dist_matrix)
-    # create_n_trees(data, dist_matrix)
+    create_one_tree_prim(data, dist_matrix, 100)
+    # create_n_trees_prim(data, dist_matrix, 100)
+    # create_n_trees_kruskal(data, dist_matrix)
 
 
 if __name__ == "__main__":

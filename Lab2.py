eaa171f2dffd6d4a9b7cb679aa9ec70ca7fd41f3
Sienki <zonced@gmail.com>
2019-03-23 16:35:18 +0100

    Add with_lines options to plot

--- Lab2.py
+++ Lab2.py
@@ -5,13 +5,13 @@ from Algorithms.lab2 import random_groups, greedy_algorithm
 
 
 def run():
-    neighbourhood = 50 #radius of neighbourhood
+    neighbourhood = 50  #radius of neighbourhood
     data = parse_data("data/objects20_06.data")
     dist_matrix = create_dist_function(data, lambda x1, x2: np.linalg.norm(x1 - x2))
     clusters = random_groups(data.shape[0])
-    draw_scatter(data, clusters)
+    draw_scatter(data, clusters, True)
     greedy_algorithm(clusters, dist_matrix, neighbourhood)
-    draw_scatter(data, clusters)
+    draw_scatter(data, clusters, True)
 
 if "__main__" == __name__:
     run()
--- Utilities/Plot.py
+++ Utilities/Plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def draw_scatter(points, clusters):
+def draw_scatter(points, clusters, with_lines):
     """
     It generates basic scatter plot from points
     :param points: points to show
@@ -10,8 +10,13 @@ def draw_scatter(points, clusters):
     """
     number_clusters = np.max(clusters)
     for i in range(number_clusters + 1):
-        np_group = np.argwhere(clusters == i)
-        plt.scatter(points[np_group].T[0], points[np_group].T[1])
+        np_cluster = np.argwhere(clusters == i)
+        if with_lines:
+            for indices_1 in np_cluster:
+                if indices_1 != np_cluster[0, 0]:
+                    plt.plot((points[np_cluster[0, 0]][0], points[indices_1][0][0]),
+                             (points[np_cluster[0, 0]][1], points[indices_1][0][1]))
+        plt.scatter(points[np_cluster].T[0], points[np_cluster].T[1])
     plt.show()
 
 

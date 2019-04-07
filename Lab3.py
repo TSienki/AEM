d40e70dfb84d07b7d28430c71c880fe03151d726
Sienki <zonced@gmail.com>
2019-04-07 20:15:36 +0200

    Deleted typos

--- Lab3.py
+++ Lab3.py
@@ -28,16 +28,16 @@ def run_measurements(data, dist_matrix, neighbourhood, steps_for_time_measuremen
     np.random.seed(0)
     for i in range(steps_for_time_measurements):
         clusters = random_groups(data.shape[0])
-        measurement = time_measure(run_algorithm, (clusters, dist_1, neighbourhood, "cache"))
+        measurement = time_measure(run_algorithm, (clusters, dist_1, neighbourhood, method))
         times_measurements.append(measurement)
         cost = sum(count_costs(clusters, dist_1, 20))/20
         print("Koszt dla iteracji " + str(i) + ": " + str(cost))
         costs_greedy.append(cost)
         draw_scatter(data, clusters, True)
 
-    print(f"Najmniejszy koszt dla lokalnego przeszukiwania w wersji zachłannej wynosi {min(costs_greedy)}, "
+    print(f"Najmniejszy koszt dla lokalnego przeszukiwania w wersji stromej ({method})  wynosi {min(costs_greedy)}, "
           f"największy {max(costs_greedy)}, średni {sum(costs_greedy)/len(costs_greedy)}.\n")
-    print(f"Pomiary czasu dla {steps_for_time_measurements} kroków dla algorytmu greedy to "
+    print(f"Pomiary czasu dla {steps_for_time_measurements} kroków dla algorytmu stromej to "
           f"min: {min(times_measurements)} sekund, max: {max(times_measurements)} sekund i "
           f"avg: {sum(times_measurements) / len(times_measurements)} sekund")
     # draw_scatter(data, clusters, True)
@@ -47,8 +47,8 @@ def run():
     neighbourhood = 50  #radius of neighbourhood
     data = parse_data("data/objects20_06.data")
     dist_matrix = create_dist_function(data, lambda x1, x2: np.linalg.norm(x1 - x2))
-    run_measurements(data, dist_matrix, neighbourhood, 1, method="none")
-    run_measurements(data, dist_matrix, neighbourhood, 1, method="cache")
+    run_measurements(data, dist_matrix, neighbourhood, 10, method="cache")
+    run_measurements(data, dist_matrix, neighbourhood, 10, method="none")
 
 
 if "__main__" == __name__:
